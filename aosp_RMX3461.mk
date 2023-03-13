10d696dffdbbb91e65a6e6f9f63b5df33cc8cc92
DhRV00T <ashwanic177@gmail.com>
2023-03-13 20:46:27 +0100

    AlphaDroid: Initial Bringup

--- aosp_RMX3461.mk
+++ aosp_RMX3461.mk
@@ -12,21 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oplus/RMX3461/device.mk)
 
 # Inherit some common Elixir Stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Official-ify
-IS_PHONE := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 #Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_RMX3461
+PRODUCT_NAME := lineage_RMX3461
 PRODUCT_DEVICE := RMX3461
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -42,3 +33,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := realme/RMX3461/RE54BFL1:12/RKQ1.210503.001/R.202210211219:user/release-keys
+
+# AlphaDroid
+WITH_GAPPS := true
+TARGET_CORE_GAPPS := true
+ALPHA_BUILD_TYPE := Official
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_USE_GRAPHENE_CAMERA := true
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ALPHA_MAINTAINER := Dhruv00
